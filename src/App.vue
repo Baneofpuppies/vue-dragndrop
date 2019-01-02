a8288646683464b769f35a9a88143c3e564dc4aa
Austen Goodson <austen@goodsons.net>
2019-01-01 18:23:09 -0800

    initial commit of dragndrop repository

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="drop-off">
+      <div class="box" v-drag>
+      </div>
+    </div>
+    <br>
+    <div class="drop-off">
+      <div class="box" v-drag>
+      </div>
+    </div> 
+
+
+    <button class="btn btn-primary" @click="addItem">Add Item</button>
+    <br>
+    <transition-group name="list" tag="ul" class="list-group">
+        <li class="list-group-item" 
+        v-for="(number, index) in numbers" 
+        @click="removeItem(index)" 
+        :key="number"> {{ number }} </li>
+    </transition-group>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      numbers: [1,2,3,4,5]
+    }
+  },
+  methods: {
+    addItem: function() {
+      const pos = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(pos, 0, this.numbers.length + 1)
+    },
+    removeItem: function(index) {
+      this.numbers.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+.box{
+  width: 100px;
+  height: 50px;
+  background-color: green;
+}
+
+.drop-off {
+  width: 500px;
+  height: 300px;
+  border: 2px black solid;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+// This directive allows an element to be dragged around
+Vue.directive('drag', {
+  bind(el,binding, vnode) {
+    el.addEventListener("mousedown",eventHandler)
+
+    function eventHandler(event) {
+      if(event.type == "mousedown"){
+        el.style.border="3px black solid" 
+        document.addEventListener("mousemove",eventHandler)
+        document.addEventListener("mouseup",eventHandler)
+        // use the following context to emit an event on the vue instance. This is so that the "drop" directive will know what to do 
+        //vnode.context.$emit(dragMouseDown)
+      }else if(event.type == "mousemove" ) {
+                // Get the properties of the current element when the mouse moves
+                var rect = el.getBoundingClientRect()
+                // set the position of the element to fixed so it pops out of the DOM - fixed makes sure that the position is in relation to the window
+                // so that scrolling will not affect it
+                el.style.position = "fixed"
+                // set the left and top properties dynamically so that the element follows the mousepointer around the page
+                el.style.left = parseInt(rect.left,10) + (event.clientX - parseInt(rect.left,10) - parseInt(rect.width,10)/2.)+ "px"
+                el.style.top = parseInt(rect.top,10) + (event.clientY - parseInt(rect.top,10) - parseInt(rect.height,10)/2.)+ "px"
+      }else if(event.type == "mouseup"){
+        // Remove the mousemove listener from the document object so that the element stops following the mouse
+        document.removeEventListener("mousemove", eventHandler)
+        document.removeEventListener("mouseup", eventHandler)
+        el.style.border=""
+        el.style.position= "absolute"
+        //el.style.left="initial"
+        //el.style.top="initial"
+      }
+    }
+
+    
+
+  }
+})
+
+// This directive allows draggable elements to be dropped into the indicated droppable element
+Vue.directive('drop', {
+  bind(el,binding, vnode) {
+    
+  }
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
